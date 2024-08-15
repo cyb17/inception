bae678f26f55078b2e1e4eb0433a59b15be9ead2
yachen <yabingc7@gmail.com>
2024-08-15 21:26:57 +0200

    testing

--- srcs/requirements/wordpress/tools/install-wordpress.sh
+++ srcs/requirements/wordpress/tools/install-wordpress.sh
@@ -16,7 +16,8 @@
 set -e
 
 # Télécharger et extraire WordPress
-if [ -f "$WP_DIR/wp-config.php" ]; then
+if [ -f "$WP_DIR/wp-config.php" ];
+then
     echo "wordpress already installed"
 else
     wget https://wordpress.org/latest.tar.gz -O /tmp/wordpress.tar.gz
@@ -40,22 +41,28 @@ sed -i "s/localhost/$DB_HOST/" $WP_DIR/wp-config.php
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /bin/wp-cli.phar
 
 # Installer WordPress en ligne de commande
-php /bin/wp-cli.phar core install \
-    --path=$WP_DIR \
-    --url=$WP_URL \
-    --title=$WP_TITLE \
-    --admin_user=$WP_ADMIN \
-    --admin_password=$WP_ADMIN_PASSWORD \
-    --admin_email=$WP_ADMIN_EMAIL \
-    --allow-root
+if ! php /bin/wp-cli.phar core is-installed --path="$WP_DIR" --allow-root;
+then
+    php /bin/wp-cli.phar core install \
+        --path=$WP_DIR \
+        --url=$WP_URL \
+        --title=$WP_TITLE \
+        --admin_user=$WP_ADMIN \
+        --admin_password=$WP_ADMIN_PASSWORD \
+        --admin_email=$WP_ADMIN_EMAIL \
+        --allow-root
+fi
 
-php /bin/wp-cli.phar user create \
-    $DB_USER \
-    $DB_USER_EMAIL \
-    --path=$WP_DIR \
-    --user_pass=$DB_PASSWORD \
-    --role=subscriber \
-    --allow-root
+if ! php /bin/wp-cli.phar user get "$DB_USER" --path="$WP_DIR" --allow-root > /dev/null 2>&1;
+then
+    php /bin/wp-cli.phar user create \
+        $DB_USER \
+        $DB_USER_EMAIL \
+        --path=$WP_DIR \
+        --user_pass=$DB_PASSWORD \
+        --role=subscriber \
+        --allow-root
+fi
 
 # Nettoyer
 rm -f $WP_DIR/wp-cli.phar
